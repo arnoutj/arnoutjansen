91a5a3a418881ebc5926c35ca0ea828eac188815
Arnout Jansen <info@arnoutjansen.nl>
2023-06-20 22:18:01 +0200

    add coming soon

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Arnout Jansen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Coming soon!" />
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
