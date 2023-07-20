a28869ebf5df590058f84c551f15700774412af3
Arnout Jansen <info@arnoutjansen.nl>
2023-07-20 21:40:19 +0200

    tweak preloading background image

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,16 +1,34 @@
 import Image from 'next/image';
 import backgroundImage from '../public/background-1440.png';
 
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) => (typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str));
+
 export default function Background() {
   return (
     <Image
       alt="Copyright 2023 © Arnout Jansen"
       src={backgroundImage}
       placeholder="blur"
-      quality={100}
-      fill
+      blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+      quality={50}
       sizes="100vw"
       style={{ objectFit: 'cover' }}
+      fill
+      priority
     />
   );
 }
