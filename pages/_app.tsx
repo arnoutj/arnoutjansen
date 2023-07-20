017649a0644b8cdd6e9819860de0758e7ced5984
Arnout Jansen <info@arnoutjansen.nl>
2023-07-20 21:46:28 +0200

    use @next/font for font loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Passion_One } from '@next/font/google';
 
 import '@styles/globals.css';
 
+const font = Passion_One({ weight: '400', display: 'swap', subsets: ['latin'] });
+
 function Application({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={font.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default Application;
