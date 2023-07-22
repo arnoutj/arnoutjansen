fac7bc6f5cce9209fe6b66aad1b184dfca09cde2
Arnout Jansen <info@arnoutjansen.nl>
2023-07-22 13:38:51 +0200

    blur image on load

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,32 +1,16 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import getStaticFileUrl from 'utils/getStaticFileUrl';
-
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
-
-const toBase64 = (str: string) => (typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str));
+import BlurImage from './BlurImage';
 
 export default function Background() {
   return (
-    <Image
+    <BlurImage
       alt="Copyright 2023 © Arnout Jansen"
       src={getStaticFileUrl('background-1440.png')}
-      placeholder="blur"
-      blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
       quality={50}
       sizes="100vw"
-      style={{ objectFit: 'cover', zIndex: -1 }}
+      style={{ zIndex: -1 }}
       fill
       priority
     />
---
+++ components/BlurImage.tsx
@@ -0,0 +1,17 @@
+import Image, { ImageProps } from 'next/image';
+import { useState } from 'react';
+
+export default function BlurImage({ src, ...props }: ImageProps) {
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <Image
+      {...props}
+      src={src}
+      fill
+      objectFit="cover"
+      className={`duration-1000 ease-in-out', ${isLoading ? 'grayscale blur-2xl scale-105' : 'grayscale-0 blur-0 scale-100'}`}
+      onLoadingComplete={() => setLoading(false)}
+    />
+  );
+}
