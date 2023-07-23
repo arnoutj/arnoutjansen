52c1c50ba6fe16a708688949c0bedea77e56bc53
Arnout Jansen <info@arnoutjansen.nl>
2023-07-23 13:36:46 +0200

    use svgr for svgs

--- next.config.js
+++ next.config.js
@@ -2,11 +2,20 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "arnoutj-website.s3.eu-central-1.amazonaws.com",
-        port: "",
-        pathname: "/assets/**"
+        protocol: 'https',
+        hostname: 'arnoutj-website.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/assets/**'
       }
     ]
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
   }
 };
