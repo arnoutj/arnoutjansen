58da3d859163476cb27564b7b3e4b1145e4d9e0f
Arnout Jansen <info@arnoutjansen.nl>
2023-07-24 20:23:24 +0200

    fade-in blur image

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -10,7 +10,7 @@ export default function BlurImage({ src, ...props }: ImageProps) {
       src={src}
       fill
       className={`object-cover duration-1000 ease-in-out', ${
-        isLoading ? "grayscale blur-2xl scale-105" : "grayscale-0 blur-0 scale-100"
+        isLoading ? "grayscale blur-2xl scale-105 opacity-0" : "grayscale-0 blur-0 scale-100 opacity-1"
       }`}
       onLoadingComplete={() => setLoading(false)}
     />
