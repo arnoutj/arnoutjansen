a5e41ab8c6a463b24dd8f9420532364e2b148df8
Arnout Jansen <info@arnoutjansen.nl>
2023-07-25 13:15:02 +0200

    fix background z-index

--- components/Background.tsx
+++ components/Background.tsx
@@ -3,14 +3,15 @@ import BlurImage from "./BlurImage";
 
 export default function Background() {
   return (
-    <BlurImage
-      alt="Copyright 2023 © Arnout Jansen"
-      src={getStaticFileUrl("background-1440.png")}
-      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAC/SURBVHgBjU85DoNAEPOy4UbQINEG+AEvzBt5AC0l4hCHODbrqVLG0kgz4/HaqxzHMb7voygK9H2Puq4xTROWZcE4jsjzHHEc4+V5HpqmgRWAPWvfdyGNMUjTFGEY4pVlGc7zlAVfoaiqKnRdB2JdV5RlCWWVhsQwDFBKIUkSOWTfti2CIMC2bdDP83xocRyHLK/rEoJg1vu+Mc8zlJ0Nc0VRJGWFcsQYruvKIcs6KEMbrbXY/YIz3fgHMm/8gS8aRFgdPOYSbgAAAABJRU5ErkJggg=="
-      quality={50}
-      sizes="100vw"
-      style={{ zIndex: -1 }}
-      priority
-    />
+    <div className="-z-10">
+      <BlurImage
+        alt="Copyright 2023 © Arnout Jansen"
+        src={getStaticFileUrl("background-1440.png")}
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAC/SURBVHgBjU85DoNAEPOy4UbQINEG+AEvzBt5AC0l4hCHODbrqVLG0kgz4/HaqxzHMb7voygK9H2Puq4xTROWZcE4jsjzHHEc4+V5HpqmgRWAPWvfdyGNMUjTFGEY4pVlGc7zlAVfoaiqKnRdB2JdV5RlCWWVhsQwDFBKIUkSOWTfti2CIMC2bdDP83xocRyHLK/rEoJg1vu+Mc8zlJ0Nc0VRJGWFcsQYruvKIcs6KEMbrbXY/YIz3fgHMm/8gS8aRFgdPOYSbgAAAABJRU5ErkJggg=="
+        quality={50}
+        sizes="100vw"
+        priority
+      />
+    </div>
   );
 }
--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -12,7 +12,7 @@ export default function BlurImage({ src, blurDataURL, ...props }: ImageProps) {
       placeholder="blur"
       layout="fill"
       blurDataURL={blurDataURL}
-      className={`object-cover duration-1000 ease-in-out', ${
+      className={`object-cover duration-1000 ease-in-out ${
         isLoading ? "grayscale blur-2xl scale-105" : "grayscale-0 blur-0 scale-100"
       }`}
       onLoadingComplete={() => setLoading(false)}
