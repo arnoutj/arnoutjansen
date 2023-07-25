9a85491c9d799a036e4a09599037f662dafcb442
Arnout Jansen <info@arnoutjansen.nl>
2023-07-25 13:06:30 +0200

    add blur base64 image for preloading background

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { useState } from "react";
 import getStaticFileUrl from "utils/getStaticFileUrl";
 import BlurImage from "./BlurImage";
 
@@ -8,10 +6,10 @@ export default function Background() {
     <BlurImage
       alt="Copyright 2023 © Arnout Jansen"
       src={getStaticFileUrl("background-1440.png")}
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAC/SURBVHgBjU85DoNAEPOy4UbQINEG+AEvzBt5AC0l4hCHODbrqVLG0kgz4/HaqxzHMb7voygK9H2Puq4xTROWZcE4jsjzHHEc4+V5HpqmgRWAPWvfdyGNMUjTFGEY4pVlGc7zlAVfoaiqKnRdB2JdV5RlCWWVhsQwDFBKIUkSOWTfti2CIMC2bdDP83xocRyHLK/rEoJg1vu+Mc8zlJ0Nc0VRJGWFcsQYruvKIcs6KEMbrbXY/YIz3fgHMm/8gS8aRFgdPOYSbgAAAABJRU5ErkJggg=="
       quality={50}
       sizes="100vw"
       style={{ zIndex: -1 }}
-      fill
       priority
     />
   );
--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -1,16 +1,19 @@
-import Image, { ImageProps } from "next/image";
+import Image, { ImageProps } from "next/legacy/image";
 import { useState } from "react";
 
-export default function BlurImage({ src, ...props }: ImageProps) {
+export default function BlurImage({ src, blurDataURL, ...props }: ImageProps) {
   const [isLoading, setLoading] = useState(true);
 
   return (
     <Image
       {...props}
       src={src}
-      fill
+      objectFit="cover"
+      placeholder="blur"
+      layout="fill"
+      blurDataURL={blurDataURL}
       className={`object-cover duration-1000 ease-in-out', ${
-        isLoading ? "grayscale blur-2xl scale-105 opacity-0" : "grayscale-0 blur-0 scale-100 opacity-1"
+        isLoading ? "grayscale blur-2xl scale-105" : "grayscale-0 blur-0 scale-100"
       }`}
       onLoadingComplete={() => setLoading(false)}
     />
