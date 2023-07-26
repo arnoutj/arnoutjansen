9d2442522e1d73c91240015c81a6acc12110e466
Arnout Jansen <info@arnoutjansen.nl>
2023-07-26 20:55:44 +0200

    add data-cookiecategory to google analytics scripts to allow blocking

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -6,8 +6,11 @@ export default function GoogleAnalytics() {
   if (process.env.NODE_ENV === "development") return null;
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
-      <Script id="google-analytics">
+      <Script
+        data-cookiecategory="analytics"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script data-cookiecategory="analytics" id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
