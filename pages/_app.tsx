ffa61da4acfe023b6e4d6c1cd93e7c8db5281851
Arnout Jansen <info@arnoutjansen.nl>
2023-07-26 20:54:35 +0200

    fix loading google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import { Alegreya } from "next/font/google";
 
 import "@styles/globals.css";
 import CookieConsent from "@components/CookieConsent";
+import GoogleAnalytics from "@components/GoogleAnalytics";
 
 function Application({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <CookieConsent />
+      <GoogleAnalytics />
     </>
   );
 }
