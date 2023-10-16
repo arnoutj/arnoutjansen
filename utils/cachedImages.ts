809c6cbc3234bc31eb923987c83abb504abdb8f8
Arnout Jansen <info@arnoutjansen.nl>
2023-10-16 18:13:29 +0200

    improve readability

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -5,16 +5,14 @@ const cloudinaryImageCache = new Map<string, any>();
 export default async function getCloudinaryFolder(cloudinaryFolder: string) {
   let cachedResults = cloudinaryImageCache.get(cloudinaryFolder);
 
-  if (!cachedResults) {
-    const fetchedResults = await cloudinary.v2.search
-      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/${cloudinaryFolder}/*`)
-      .sort_by("public_id", "desc")
-      .max_results(100)
-      .execute();
+  if (cachedResults) return cachedResults;
 
-    cloudinaryImageCache.set(cloudinaryFolder, fetchedResults);
-    return fetchedResults;
-  }
+  const fetchedResults = await cloudinary.v2.search
+    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/${cloudinaryFolder}/*`)
+    .sort_by("public_id", "desc")
+    .max_results(100)
+    .execute();
 
-  return cachedResults;
+  cloudinaryImageCache.set(cloudinaryFolder, fetchedResults);
+  return fetchedResults;
 }
