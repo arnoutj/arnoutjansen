023c3eacb8aef3908adc3bb08b91426bd60bee7c
Arnout Jansen <info@arnoutjansen.nl>
2023-10-16 18:12:43 +0200

    fix typo

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -7,7 +7,7 @@ export default async function getCloudinaryFolder(cloudinaryFolder: string) {
 
   if (!cachedResults) {
     const fetchedResults = await cloudinary.v2.search
-      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/${cloudinaryFolder}*`)
+      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/${cloudinaryFolder}/*`)
       .sort_by("public_id", "desc")
       .max_results(100)
       .execute();
