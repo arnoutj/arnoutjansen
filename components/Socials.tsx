55562cf44ad740f14733bf07ae85aac5ec536b06
Arnout Jansen <info@arnoutjansen.nl>
2023-12-09 13:44:42 +0100

    add spotify icon

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -2,6 +2,7 @@ import InstagramIcon from "../public/icons/instagram.svg";
 import TiktokIcon from "../public/icons/tiktok.svg";
 import FacebookIcon from "../public/icons/facebook.svg";
 import BandcampIcon from "../public/icons/bandcamp.svg";
+import SpotifyIcon from "../public/icons/spotify.svg";
 
 import Link from "next/link";
 
@@ -14,11 +15,14 @@ export default function Socials({ className }: { className?: string }) {
       <Link href="https://www.instagram.com/arnoutjansen_" target="blank" title="Instagram" className={linkClassname}>
         <InstagramIcon className="w-full h-full" alt="Instagram" />
       </Link>
-      <Link href="https://www.tiktok.com/@arnoutjansen_" target="blank" title="Tiktok" className={linkClassname}>
+      {/* <Link href="https://www.tiktok.com/@arnoutjansen_" target="blank" title="Tiktok" className={linkClassname}>
         <TiktokIcon className="w-full h-full" alt="Tiktok" />
-      </Link>
-      <Link href="https://www.facebook.com/arnoutjansenmusic" target="blank" title="Facebook" className={linkClassname}>
+      </Link> */}
+      {/* <Link href="https://www.facebook.com/arnoutjansenmusic" target="blank" title="Facebook" className={linkClassname}>
         <FacebookIcon className="w-full h-full" alt="Facebook" />
+      </Link> */}
+      <Link href="#" target="blank" title="Spotify" className={linkClassname}>
+        <SpotifyIcon className="w-full h-full" alt="Spotify" />
       </Link>
       <Link href="https://arnoutjansen.bandcamp.com" target="blank" title="Bandcamp" className={linkClassname}>
         <BandcampIcon className="w-full h-full" alt="Bandcamp" />
